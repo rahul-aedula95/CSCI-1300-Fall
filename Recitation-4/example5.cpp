efa4e2fdcd1fa0fe5c8d69c7ef4a10ed3285227f
Rahul Aedula <rahulaedula95@gmail.com>
2021-09-13 15:21:34 -0600

    Recitation 4

---
+++ Recitation-4/example5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int age; 
+
+    cout <<"What is the temperature?"<<endl;
+
+    cin >> age;
+
+    if(age>=90){
+        cout<<"It is hot outside"<<endl;
+
+    }
+
+    else{
+        cout<<"It is cold outside"<<endl;
+    }
+
+}
