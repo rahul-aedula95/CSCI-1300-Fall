592233a354936a1f192e0db01177e0abc081ea0e
Rahul Aedula <rahulaedula95@gmail.com>
2021-09-13 15:00:56 -0600

    changed names from week to recitation

--- Recitation-2/helloWorld.cpp
+++ Recitation-2/helloWorld.cpp
@@ -6,3 +6,4 @@ int main(){
     cout<<"HELLO WORLD"<<endl;
 }
 
+
---
+++ Recitation-2/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+int var = 10;
+
+cout << "The variable is " << var << endl;
+
+var = 20;
+
+cout << "The variable is " << var << endl;
+
+}
