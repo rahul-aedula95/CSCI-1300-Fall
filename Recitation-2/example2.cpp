7d1d601873801c765300593a54e34954abf3bb26
Rahul Aedula <rahulaedula95@gmail.com>
2021-10-01 12:20:57 -0600

    recitation 2 update

--- Recitation-2/example2.cpp
+++ Recitation-2/example2.cpp
@@ -3,6 +3,7 @@
 // Comments are primarily used as ways to write short descriptions to help developers.
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -10,7 +11,24 @@ int main(){
 
     // Let us create some variables.
 
-    // int a = 10;
+    int a = 10;
+    cout<<"The value of a is "<< a <<endl;
+
+    char ch = 'h';
+    cout<<"The value of ch is "<< ch <<endl;
+
+    string str = "123";
+    
+    cout<<"The value of str is "<< str <<endl;
+    // str = str + 20;
+    // cout<<"The value of str is "<< str <<endl;
+
+    float b = 3.14;
+    cout<<"The value of b is "<< b <<endl;
+    b = b + 20;
+    cout<<"The value of b is "<< b <<endl;
+
+
 
     // // if you want to change the value of a
     
@@ -22,9 +40,9 @@ int main(){
 
     // You can also just initialize a variable without a value
 
-    int a; 
+    // int a; 
 
-    cout<<"The value of a is "<< a <<endl;
+    
 
     // This will fill a with a dummy value of zero
 
@@ -32,3 +50,4 @@ int main(){
 
 
 }
+
