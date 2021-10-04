e79a2826bc99e9d880f149e4d40e057e71c44b22
Rahul Aedula <rahulaedula95@gmail.com>
2021-10-04 14:54:26 -0600

    arrayFunc.cpp

--- Recitation-7/arrayFunc.cpp
+++ Recitation-7/arrayFunc.cpp
@@ -2,13 +2,23 @@
 using namespace std;
 
 void testFunc(int array[], int length);
+void testFunc2(int var);
 
 int main(){
 
     int length = 5;
     int arr[length] = {10,20,30,40,50};
+    int var = 30;
+
+    // calling testFunc 2 to show that other variables are unaffected
+    testFunc2(var);
+    cout << "Value of var " <<var<<endl;
 
     testFunc(arr, length);
+
+        
+
+
 
     //  // first we will just attempt to print it
     // for(int i=0;i<length;i++){
@@ -36,4 +46,10 @@ void testFunc(int array[], int length){
     //     array[i] = array[i] + 1;
         
     // }
+}
+
+
+void testFunc2(int var){
+
+    var = 40;
 }
